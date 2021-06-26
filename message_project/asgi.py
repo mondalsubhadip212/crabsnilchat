8eacc0983a7943d731640e0235eab5ba0e436153
Subhadip Mondal <74086329+mondalsubhadip212@users.noreply.github.com>
2021-06-26 11:36:47 +0530

    csdcsdc

--- message_project/asgi.py
+++ message_project/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_project.settings')
+
 
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import crabsnil_chat.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
