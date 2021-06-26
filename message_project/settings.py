9c24fd2e6e64027fb8c9418c1f2ad9cf85327b27
Subhadip Mondal <74086329+mondalsubhadip212@users.noreply.github.com>
2021-06-26 12:09:45 +0530

    sas

--- message_project/settings.py
+++ message_project/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# import django_heroku
 # import dj_database_url
 # import os
 import django_heroku
 from pathlib import Path
-
 from datetime import timedelta
 
 # import channels_redis
@@ -181,5 +179,4 @@ CHANNEL_LAYERS = {
     },
 }
 
-import django_heroku
 django_heroku.settings(locals())
