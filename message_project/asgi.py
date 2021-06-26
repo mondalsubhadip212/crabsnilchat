2aded7d544e391e393f23b5a262744d512662dec
Subhadip Mondal <74086329+mondalsubhadip212@users.noreply.github.com>
2021-06-26 12:41:50 +0530

    xasxasx

--- message_project/asgi.py
+++ message_project/asgi.py
@@ -8,30 +8,23 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-
-# Fetch Django ASGI application early to ensure AppRegistry is populated
-# before importing consumers and AuthMiddlewareStack that may import ORM
-# models.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "message_project.settings")
 django_asgi_app = get_asgi_application()
 
 
-# 
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
-
+import crabsnil_chat.routing
 
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     # Just HTTP for now. (We can add other protocols later.)
-    "websocket" :
-     AuthMiddlewareStack(
+    "websocket" : AuthMiddlewareStack(
         URLRouter(
             crabsnil_chat.routing.websocket_urlpatterns
         )
     )
 })
+
