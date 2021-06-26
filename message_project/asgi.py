bb1d82cc909bbd8c914c7547d598b6e75a42fc24
Subhadip Mondal <74086329+mondalsubhadip212@users.noreply.github.com>
2021-06-26 12:37:41 +0530

    dadad

--- message_project/asgi.py
+++ message_project/asgi.py
@@ -20,7 +20,7 @@ django_asgi_app = get_asgi_application()
 
 
 # 
-from channels.routing import ProtocolTypeRouter,URLRouter, get_asgi_application
+from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 
 
